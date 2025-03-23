ac612ff7b83f2a4030add0ff4d9833e0407bd6ce
M-R-YOGANANDA <yoganandamr7002@gmail.com>
2025-03-23 20:04:15 +0530

    Create type cast.py

---
+++ type cast.py
@@ -0,0 +1,14 @@
+#type casting 
+#converting a data from one data type another 
+#int -> str
+a=23
+print(str(a))
+#float -> str
+a=43.00
+print(str(a))
+#float -> int 
+a=34.485
+print(int(a))
+#int -> float
+a=3534
+#print(float(a))
