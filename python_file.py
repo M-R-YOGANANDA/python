47570c5e03527325bdc983a5be239d9e150ef67b
M-R-YOGANANDA <yoganandamr7002@gmail.com>
2025-03-27 23:59:31 +0530

    Create python_file.py

---
+++ python_file.py
@@ -0,0 +1,7 @@
+# Creating and writing to a Python file
+with open("script.py", "w") as file:
+    file.write("""# This is a sample Python script
+print("Hello, World!")  
+""")
+
+print("Python file 'script.py' has been created successfully.")
